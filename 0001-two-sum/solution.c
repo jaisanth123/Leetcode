faea608d6dfc6258b5c81a394c51cec8653bb18d
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2024-02-08 06:11:26 +0000

    [LeetCode Sync] Runtime - 64 ms (84.62%), Memory - 6.1 MB (100.00%)

--- 0001-two-sum/solution.c
+++ 0001-two-sum/solution.c
@@ -1,16 +1,21 @@
-int* twoSum(int* nums, int numsSize, int target, int* returnsize) {
-int i, j;
-int* result = (int*)malloc(2 * sizeof(int));
-for (i=0; i < numsSize; i++) {
-for (j=i+1; j < numsSize; j++) {
-if (nums[i] + nums[j] == target) {
-result[0] = i;
-result[1] = j;
-*returnsize= 2;
-return result;
-}
-}
-}
-return NULL;
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int* result = (int*)malloc(2 * sizeof(int));
+    int numToIndex[numsSize][2];
+
+    for (int i = 0; i < numsSize; ++i) {
+        int complement = target - nums[i];
+        for (int j = 0; j < i; ++j) {
+            if (numToIndex[j][0] == complement) {
+                result[0] = j;
+                result[1] = i;
+                *returnSize = 2;
+                return result;
+            }
+        }
+        numToIndex[i][0] = nums[i];
+        numToIndex[i][1] = i;
+    }
+    
+    return result;
 }
 
