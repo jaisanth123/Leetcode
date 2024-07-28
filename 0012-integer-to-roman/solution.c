16253937ad0dbe1fda9a924fba62c2bafe16ba63
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2024-07-28 13:01:18 +0000

    [LeetCode Sync] Runtime - 6 ms (11.89%), Memory - 9.3 MB (17.02%)

--- 0012-integer-to-roman/solution.c
+++ 0012-integer-to-roman/solution.c
@@ -1,17 +1,17 @@
 char* intToRoman(int num) {
     int numbers[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    const char *romanNumerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    const char *rNls[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-    char* romanNumeral = (char*)malloc(20 * sizeof(char));  // Allocate memory for the result
-    romanNumeral[0] = '\0';  // Initialize the string to an empty string
+    char* rNl = (char*)malloc(20 * sizeof(char));  
+    rNl[0] = '\0';
 
     for (int i = 0; i < 13; i++) {
         while (num >= numbers[i]) {
-            strcat(romanNumeral, romanNumerals[i]);
+            strcat(rNl, rNls[i]);
             num -= numbers[i];
         }
     }
-    return romanNumeral;  // Return the constructed roman numeral
+    return rNl; 
 }
 
 
