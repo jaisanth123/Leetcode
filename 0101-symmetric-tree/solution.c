f97ad9b345c2747067833ed53f4433a2f759c8a5
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2024-07-28 12:55:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.1 MB (94.77%)

---
+++ 0101-symmetric-tree/solution.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSymmetric(struct TreeNode* root) {
+    bool isMirror(struct TreeNode*left , struct TreeNode*right){
+        if (left == NULL && right == NULL){
+            return true;
+        }
+        if (left == NULL || right == NULL){
+            return false;
+        }
+        return (left -> val==right->val) && isMirror(left->left , right->right)
+         && isMirror(left->right , right->left );
+    }
+    if(root==NULL){
+        return true;
+    }
+    return (isMirror(root->left , root->right));
+    
+    
+    }
