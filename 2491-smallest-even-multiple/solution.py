85123e32eb2f40ead194c323b4c8324fa0f7953e
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2024-07-29 16:58:16 +0000

    [LeetCode Sync] Runtime - 36 ms (0.34%), Memory - 16.5 MB (100.00%)

--- 2491-smallest-even-multiple/solution.py
+++ 2491-smallest-even-multiple/solution.py
@@ -1,7 +1,3 @@
-class Solution(object):
-    def smallestEvenMultiple(self, n):
-            if (n%2==0):
-                return n
-            else:
-                return n*2
-        
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        return n if n % 2 == 0 else n * 2
