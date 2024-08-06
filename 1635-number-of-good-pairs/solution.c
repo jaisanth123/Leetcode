c12934a708f3361d7c1fb179cbd7c93ad5f61c03
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2024-08-06 08:02:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8 MB (51.10%)

--- 1635-number-of-good-pairs/solution.c
+++ 1635-number-of-good-pairs/solution.c
@@ -1,10 +1,8 @@
 int numIdenticalPairs(int* nums, int numsSize) {
     int goodPairs = 0;
-
-    // Iterate through each pair of elements in the array
     for (int i = 0; i < numsSize - 1; i++) {
         for (int j = i + 1; j < numsSize; j++) {
-            // Check if the pair is good
+        
             if (nums[i] == nums[j]) {
                 goodPairs++;
             }
@@ -13,4 +11,3 @@ int numIdenticalPairs(int* nums, int numsSize) {
 
     return goodPairs;
 }
-
