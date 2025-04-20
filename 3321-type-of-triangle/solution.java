e50b357cd04cb7a03b4c7e2a5ed8eddcef180907
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-04-20 06:37:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (43.45%)

---
+++ 3321-type-of-triangle/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        int a = nums[0] , b = nums[1],c=nums[2];
+        if(a+b > c && a+c > b && b+c > a){
+            if(a==b && b==c) return "equilateral";
+            else if(a==b || b==c || c==a ) return "isosceles";
+            else return "scalene";
+        }
+        return "none";
+        
+    }
+}
