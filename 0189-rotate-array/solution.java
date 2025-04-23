deb32d2c222d59466ca1cff162d32c8fb28e21b7
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-04-23 06:37:53 +0000

    [LeetCode Sync] Runtime - 1 ms (48.07%), Memory - 57.2 MB (85.64%)

--- 0189-rotate-array/solution.java
+++ 0189-rotate-array/solution.java
@@ -1,21 +1,11 @@
 class Solution {
     public void rotate(int[] nums, int k) { 
         int n = nums.length;
-        int rot = k%n;
-        rotateArray(nums,0,n-1);
-        rotateArray(nums,0,rot-1);
-        rotateArray(nums,rot,n-1);
-    
-    }
-
-    public int[] rotateArray(int []arr , int start , int stop){
-        while (start<stop){
-            int temp = arr[start];
-            arr[start] = arr[stop];
-            arr[stop] = temp ;
-            start++;
-            stop--;
+        int  []res = new int [n];
+        System.arraycopy(nums,0,res,0,n);
+        for(int i = 0 ; i < n ; i++){
+                 nums[(i+k)%n]   =res[i];
         }
-        return arr;
+        
     }
 }
