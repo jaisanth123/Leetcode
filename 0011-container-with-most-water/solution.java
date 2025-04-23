302ac7b241320e12a25206321c927eb88907e805
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-04-23 08:08:21 +0000

    [LeetCode Sync] Runtime - 6 ms (7.04%), Memory - 57.8 MB (64.42%)

--- 0011-container-with-most-water/solution.java
+++ 0011-container-with-most-water/solution.java
@@ -1,20 +1,16 @@
 class Solution {
     public int maxArea(int[] height) {
-        int left = 0 ;
-        int right =  height.length-1;
-        int area = 0 ;
-        while (left<right){
-            int width = right - left;
+        int left = 0 , right = height.length-1 , area = 0;
+        while (left < right){
             int h = Math.min(height[left] , height[right]);
-            area = Math.max(area , width*h);
-
-            if(height[left]<height[right]){
+            int width = right - left;
+            area = Math.max(area, width*h);
+            if(height[left] < height[right])
                 left++;
-            }
-            else{
+            else
                 right--;
-            }
-        }
+        }    
         return area;
+        
     }
 }
