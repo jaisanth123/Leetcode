7feacac0a0ee09299155c94cc6677f1518f2c932
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-04-27 12:10:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.8 MB (19.69%)

--- 1444-number-of-steps-to-reduce-a-number-to-zero/solution.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/solution.java
@@ -1,13 +1,12 @@
 class Solution {
     public int numberOfSteps(int num) {
-    int c = 0 ;
-    while (true){
-        if(num ==0 ){
-            break;
+        int c=0;
+        while (true){
+            if(num==0)
+                break;
+            num = (num%2==0)?num/2:num-1;
+            c++;
         }
-       num= (num%2==0)?(num/2):(num-1);
-        c++;
-    }    
-    return c;
+        return c;
     }
 }
