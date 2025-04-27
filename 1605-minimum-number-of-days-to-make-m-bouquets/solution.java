8e5bede9080da9a62f25998a2f04ea0eb78a3e6d
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-04-27 04:05:25 +0000

    [LeetCode Sync] Runtime - 16 ms (93.22%), Memory - 58.7 MB (26.39%)

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/solution.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        int n = bloomDay.length;
+        if(n<((long)m*k))
+            return -1;
+    
+        int left = Integer.MAX_VALUE;
+        int right = Integer.MIN_VALUE;
+        for(int i : bloomDay){
+            if(i<left)
+                left=i;
+            if(i>right)
+                right=i;
+        }
+        int ans = right;
+        while (left <= right){
+            int mid = (left+right) /2;
+            if(possible(bloomDay,mid,m,k)){
+                ans = mid ;
+                right = mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return ans;
+    }
+
+    //     for(int i = left ; i <=right ; i++){
+    //         if(possible(bloomDay,i,m,k)){
+    //             return i;
+    //         }
+    //     }
+    // return -1;
+    // }
+    
+
+    private boolean possible (int []arr , int day , int m , int k){
+        int counter =0,total=0 ;
+        for(int i : arr){
+            if(i<=day){
+                counter++;
+                if(counter==k){
+                    total++;
+                    counter=0;
+                }
+                }
+            else{
+                counter=0;
+            }
+        }
+        return total>=m;
+    }
+}
