80909216f340d08b7e2da6559ce06e69b0536fba
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-04-27 12:19:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.5 MB (39.06%)

--- 1642-water-bottles/solution.java
+++ 1642-water-bottles/solution.java
@@ -1,11 +1,11 @@
 class Solution {
     public int numWaterBottles(int nb, int ne) {
-        int c = nb;
-        int ep = nb ;
-        while (ep >= ne ){
-            c+=ep/ne;
-            ep = ep % ne + ep/ne;
-   }
-   return c;
-   }
+        int cb = nb;
+    
+        while(nb>=ne){
+            cb+=nb/ne;
+            nb= nb%ne + nb/ne;
+        }
+        return cb;
+    }
 }
