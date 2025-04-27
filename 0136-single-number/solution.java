dae9b0527e58513f03e356348cbc6a55412eb46a
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-04-27 14:45:23 +0000

    [LeetCode Sync] Runtime - 1 ms (99.92%), Memory - 46.5 MB (26.06%)

--- 0136-single-number/solution.java
+++ 0136-single-number/solution.java
@@ -1,22 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap <Integer,Integer>hm=new HashMap<>();
-        for(int i:nums){
-        if(hm.containsKey(i)){
-            int val=hm.get(i);
-            hm.put(i,val+1);
+        int x=0;
+        for(int i:nums ){
+            x^=i;
         }
-        else{
-            hm.put(i,1);
-        }
-    }
-    int t =0;
-    for(int i:nums ){
-        if(hm.get(i)==1){
-            t =i;
-        }
-    }
-    return t ;
+        return x;
         
-    }}
-
+    }
+}
