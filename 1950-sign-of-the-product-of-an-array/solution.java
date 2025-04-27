0aae578b0e0bbe9df6cefc5ec21c364e8c1bb35a
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-04-27 14:54:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.6 MB (5.79%)

--- 1950-sign-of-the-product-of-an-array/solution.java
+++ 1950-sign-of-the-product-of-an-array/solution.java
@@ -1,18 +1,14 @@
 class Solution {
     public int arraySign(int[] nums) {
-        int nc=0;
-        for(int i : nums){
-            if (i == 0)
-             return 0;
-            if(i<0) nc++;
+        int nve=0;
+        for(int i:nums){
+            if(i==0)
+                return 0;
+            if(i<0)
+                nve++;
         }
-
-        if(nc%2 ==0)
-        return 1;
-        else
+        if(nve%2==0)
+            return 1;
         return -1;
-
-
-        
     }
 }
