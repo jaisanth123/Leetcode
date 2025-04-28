219edaa48a5e54676f929a895cf4671184b6303f
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-04-28 05:09:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.4 MB (66.43%)

--- 0461-hamming-distance/solution.java
+++ 0461-hamming-distance/solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int count =0;
-        int n = x ^ y;
-        while (n !=0){
-            count += n&1;
-            n >>=1;
-        }
-        return count;
-        
+        int count = 0;
+        int n = x^y;
+       return  Integer.bitCount(x^y);
+        // while (n!=0){
+        //     count += n&1;
+        //     n>>=1;
+        // }
+        // return  count;
     }
 }
