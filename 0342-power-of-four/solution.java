fc3ecc08314e743afa80c814548e7afe2685cf5e
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-04-28 05:02:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.6 MB (89.06%)

--- 0342-power-of-four/solution.java
+++ 0342-power-of-four/solution.java
@@ -1,11 +1,7 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-     if (n <=0) return false ;
-     return (n & (n-1 )) == 0 && (n&0x55555555 ) !=0 ;
+        
+if (n <=0) return false ;
+     return (n & (n-1 )) == 0 && (n-1) %3==0 ;
     }
 }
-
-
-
-
-
