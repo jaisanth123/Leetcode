2146e3f2a54eb3f633b60a9232b28ae8619ac006
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-04-28 02:50:46 +0000

    [LeetCode Sync] Runtime - 1 ms (99.22%), Memory - 41.8 MB (78.39%)

--- 0389-find-the-difference/solution.java
+++ 0389-find-the-difference/solution.java
@@ -1,16 +1,12 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-
-    int r = 0;
-
-    for (char c : s.toCharArray()){
-        r^=c;
-    }
-    for(char c : t.toCharArray() ){
-        r^=c;
-    }
-
-    return (char)r;
+        int res =0;
+        for(char c:s.toCharArray())
+            res^=c;
         
+        for(char c:t.toCharArray())
+            res^=c;
+        
+        return (char)res;
     }
-}
+    }
