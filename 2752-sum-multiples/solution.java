e54081b7de4ab5a04a8670b0b310a55b996710a2
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-05-01 14:25:52 +0000

    [LeetCode Sync] Runtime - 3 ms (63.87%), Memory - 41.3 MB (21.00%)

--- 2752-sum-multiples/solution.java
+++ 2752-sum-multiples/solution.java
@@ -1,11 +1,10 @@
 class Solution {
     public int sumOfMultiples(int n) {
-        int count=0;
-        for(int i = 1 ; i <= n ; i++){
-            if(i%3==0 || i%5==0 || i%7==0){
-                count+=i;
-            }
-        }   
-        return count;
+       int sum = 0 ;
+       for(int i = 1; i<=n ; i++){
+        if(i%3==0 || i%5==0 || i%7 == 0)
+            sum+=i;
+       }
+       return sum;
     }
 }
