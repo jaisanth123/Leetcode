c38b56fc37a19fdf6b890c04bffdb29aed3e7753
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-07-30 00:55:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 49.7 MB (11.44%)

--- 0344-reverse-string/solution.java
+++ 0344-reverse-string/solution.java
@@ -1,15 +1,10 @@
 class Solution {
     public void reverseString(char[] s) {
-      
-        int l = 0;
-        int r = s.length-1;
-        while(l<r){
-            char temp = s[l];
-            s[l] = s[r];
-            s[r] = temp;
-
-            l++;
-            r--;
-        }
+        int l = 0 , r = s.length-1;
+        while (l<r){
+            char t = s[l];
+            s[l++] = s[r];
+            s[r--] = t;        
+        }    
     }
 }
