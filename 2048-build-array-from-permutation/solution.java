88a9f73c5f1d8d9c35abb16d201c3aabd8daede4
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-08-05 08:41:55 +0000

    [LeetCode Sync] Runtime - 1 ms (99.07%), Memory - 45.3 MB (81.91%)

--- 2048-build-array-from-permutation/solution.java
+++ 2048-build-array-from-permutation/solution.java
@@ -1,12 +1,9 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-
         int [] arr = new int [nums.length];
-        for(int i = 0 ; i < nums.length ; i++){
-            arr[i] = nums[nums[i]];
-        }
-
-        return arr;
+        for(int i =0 ; i < nums.length ; i++)
+        arr[i] = nums[nums[i]];
         
+        return arr;
     }
 }
