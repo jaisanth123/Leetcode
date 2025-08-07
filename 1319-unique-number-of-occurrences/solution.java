b542ebd1ffe8b6d3ef274b8247592008ea110f5c
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-08-07 08:23:18 +0000

    [LeetCode Sync] Runtime - 2 ms (98.00%), Memory - 41.8 MB (69.37%)

--- 1319-unique-number-of-occurrences/solution.java
+++ 1319-unique-number-of-occurrences/solution.java
@@ -1,14 +1,11 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-    HashMap <Integer , Integer> map = new HashMap <>();
-    for(int i : arr){
-        map.put(i,map.getOrDefault(i,0)+1);
+       HashMap <Integer , Integer> map = new HashMap<>();
+       for(int i : arr)
+        map.put(i , map.getOrDefault(i,0)+1);
+
+        HashSet<Integer> set = new HashSet<>(map.values());
+
+        return (map.size() == set.size()) ;
     }
-    HashSet <Integer> set = new HashSet<>(map.values());
-
-    if(set.size()==map.size())
-        return true ;
-    return false;
-
-       }
 }
