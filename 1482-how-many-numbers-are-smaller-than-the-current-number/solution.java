45113cded24d8fabfa78f52ac88b8793a02bcf68
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-08-13 00:21:08 +0000

    [LeetCode Sync] Runtime - 1 ms (99.64%), Memory - 44.3 MB (89.51%)

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int res[] = new int [nums.length]; 
+        int count [] = new int [101];
+        for(int n : nums){
+            count[n]++;
+        }
+        for(int i=1 ;i < count.length ;i++)
+            count[i] += count[i-1];
+
+        for(int i = 0 ; i <nums.length;i++){
+            if(nums[i]==0)
+                res[i] =0;
+            else
+                res[i] = count[nums[i] -1];         
+        } 
+
+        return res;
+
+                      }
+                      
+    }
+
