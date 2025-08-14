2982e11add0b0f686366d1f259f7bb465a56d18a
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-08-14 06:24:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.6 MB (62.42%)

--- 1797-goal-parser-interpretation/solution.java
+++ 1797-goal-parser-interpretation/solution.java
@@ -1,17 +1,19 @@
 class Solution {
     public String interpret(String c) {
-     StringBuilder str = new StringBuilder ();;
-     boolean flag = false;
-     for (char ch : c.toCharArray()){
-        if(ch <= 'z' && ch >= 'a' || ch <= 'Z' && ch >= 'A'){
-            str.append(ch);
-            flag = false;}
-        if(ch == '(')
-            flag = true;
-        if(ch ==')' && flag)
-            str.append('o');
-     }
-     return str.toString();
+        StringBuilder str = new StringBuilder();
+        boolean flag = false;
+        for(char ch : c.toCharArray()){
+            if(ch >= 'A' && ch<= 'Z' || ch >='a' && ch<= 'z' ){
+                str.append(ch);
+                flag=false;
+            }
+            else if(ch=='(')
+                flag = true;
+            else if(ch==')' && flag){
+                str.append('o');
+            }
 
+        }
+        return str.toString();
     }
 }
