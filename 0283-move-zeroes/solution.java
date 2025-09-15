e74f016d5e817cddf58e2552686496cc3e08eada
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-09-15 06:51:42 +0000

    [LeetCode Sync] Runtime - 1 ms (99.64%), Memory - 46 MB (79.46%)

--- 0283-move-zeroes/solution.java
+++ 0283-move-zeroes/solution.java
@@ -1,10 +1,9 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int pos = 0 ;
-        for(int i : nums){
-            if(i!=0)
-            nums[pos++] = i;
-        }
+        int pos =0;
+        for(int n : nums)
+            if(n!=0)
+                nums[pos++] = n;
         while(pos<nums.length)
             nums[pos++] =0;
     }
