afa6ad886997beb60d9a7a16419ccd638698876f
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-09-16 04:14:18 +0000

    [LeetCode Sync] Runtime - 1 ms (99.77%), Memory - 52.9 MB (67.73%)

--- 0169-majority-element/solution.java
+++ 0169-majority-element/solution.java
@@ -1,31 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap <Integer , Integer> map = new HashMap<>();
-        for(int i : nums){
-                map.put (i,map.getOrDefault(i,0)+1);
-        }
-
-        int mc = 0;
-        int mj = 0 ;
-        for(int i : map.keySet()){
-            if(map.get(i)>mc){
-                mc=map.get(i);
-                mj=i;
-            }
-        }
-        return mj;
-    
-        // int res = 0 , maj = 0;
-        // for(int i :nums){
-        //     if(maj==0)
-        //         res= i ;
-        //     if(maj==i)
-        //         maj++;
-        //     else
-        //         maj--;
-        // }
-        
-        
-        // return res;
+            int count = 0;
+            int candidate = nums[0];
+            for(int i =1 ; i<nums.length ; i++){
+                int x = nums[i];
+                if(count == 0 && x!= candidate)
+                    candidate =x;
+                else if(x == candidate)
+                    count++;
+                else 
+                    count--;
+            }    
+            return candidate;
     }
 }
