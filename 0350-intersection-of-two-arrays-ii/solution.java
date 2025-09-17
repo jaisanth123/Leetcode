10582c602f6cddcb9738292e449070c14b14807e
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-09-17 03:58:56 +0000

    [LeetCode Sync] Runtime - 3 ms (90.84%), Memory - 43.3 MB (67.98%)

---
+++ 0350-intersection-of-two-arrays-ii/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap <Integer , Integer> map= new HashMap<>();
+        for(int i : nums1)
+            map.put(i,map.getOrDefault(i,0)+1);
+        List <Integer> list= new ArrayList<>();
+        for(int i : nums2){
+            if(map.containsKey(i) && map.get(i)>0){
+                list.add(i);
+                map.put(i,map.get(i)-1);
+            }
+        }
+        int [] arr = new int [list.size()];
+        int j = 0 ;
+        for(int i: list)
+        arr[j++] = i;
+
+     return arr;
+    }
+
+}
