56a506b977125aebb9b60f009452f5739699491d
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-09-22 04:58:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.5 MB (51.19%)

--- 0070-climbing-stairs/solution.java
+++ 0070-climbing-stairs/solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-            if(n<=3)
-                return n;
-            int p1 =3;
-            int p2=2;
-            int c =0;
-            for(int i = 3 ;i<n;i++){
-                c = p1+p2;
-                p2=p1;
-                p1=c;
-            
-            }
-            return c;
-        }
+        if(n <= 2 ) return n ;
+        int [] dp = new int[n+1];
+        Arrays.fill(dp,0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3 ; i <= n ; i ++)
+            dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
+
+    }
+
 }
