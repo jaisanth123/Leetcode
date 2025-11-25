a0275b16d6dc293cb1addb1f24615891728156d9
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-11-25 23:34:51 +0000

    [LeetCode Sync] Runtime - 1 ms (83.01%), Memory - 42.9 MB (7.19%)

--- 0231-power-of-two/solution.java
+++ 0231-power-of-two/solution.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n <= 0 )
-            return false;
-        return (n&(n-1))==0;
-        
+        if(n==1) return true;
+        if(n<=0) return false;
+        if(n%2!=0) return false;
+        return isPowerOfTwo(n/2);
+    
+
     }
 }
