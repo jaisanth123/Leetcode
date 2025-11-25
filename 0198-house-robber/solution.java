bc3e520b774d41acd8b0a6254e9359f5f1ba3756
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-11-25 16:17:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43 MB (8.05%)

--- 0198-house-robber/solution.java
+++ 0198-house-robber/solution.java
@@ -1,21 +1,18 @@
 class Solution {
-    // private int helper(int nums[], int n)
-    // {
-    //     if(n<0)
-    //         return 0;
-    //     // if(memo[n]!=-1)
-    //         // return memo[n];
-    //     return  Math.max(helper(nums,n-1),nums[n]+helper( nums,n-2));
-    // }
+    private int helper(int memo[] , int nums [] , int n ){
+    
+        if(n <0)
+            return 0;
+        if(memo[n]!=-1)
+            return memo[n];
+        return memo[n]= Math.max(helper(memo,nums,n-1),nums[n]+helper(memo,nums,n-2));
+    }
+
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n==1) return nums[0];
-        int dp[] = new int [n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
-        for(int i = 2; i < n ; i++){
-            dp[i]= Math.max(dp[i-1],dp[i-2]+nums[i]);
-        }
-        return dp[n-1];
+        int memo []= new int [n+1];
+        Arrays.fill(memo,-1);
+        return helper(memo , nums ,n-1);
+        
     }
 }
