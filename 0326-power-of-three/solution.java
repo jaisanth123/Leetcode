bcea4872424b0a2380d16fd353a38259f560410d
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-11-25 23:30:54 +0000

    [LeetCode Sync] Runtime - 7 ms (100.00%), Memory - 46 MB (46.87%)

--- 0326-power-of-three/solution.java
+++ 0326-power-of-three/solution.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
+        if(n==1)
+            return true;
         if(n<=0)
             return false;
-        return(Math.pow(3,19) %n ==0);
-        
+        if(n%3 !=0) return false;
+        return isPowerOfThree(n/3);
     }
 }
