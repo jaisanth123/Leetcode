655d04972bb4816f8cf8af220dcf5b8daede5b96
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-11-30 01:40:55 +0000

    [LeetCode Sync] Runtime - 18 ms (79.55%), Memory - 97.9 MB (58.20%)

--- 0077-combinations/solution.java
+++ 0077-combinations/solution.java
@@ -1,20 +1,22 @@
 class Solution {
+    int i = 0 ; 
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> comb = new ArrayList<>();
-        backTrack(n , k , res, comb , 1);
-        return res;
-        
+        List<Integer> arr = new ArrayList<>();
+        helper (1 ,n, k , res ,arr );
+        return res ; 
     }
+    private void helper (int start ,int n , int k , List<List<Integer>> res , List<Integer> arr ){
+        if(arr.size()==k)
+        {
+            res.add(new ArrayList<>(arr));
+            return;
+        }
 
-    private void backTrack(int n , int k , List<List<Integer>>res , List<Integer>comb, int current){
-        if(comb.size() == k){
-            res.add(new ArrayList<Integer>(comb));
-            return ;}
-        for(int i = current ; i <= n ; i++ ){
-            comb.add(i);
-            backTrack(n , k , res, comb , i+1);
-            comb.remove(comb.size()-1);
+            for(int i = start ; i <= n ; i++ ){
+                arr.add(i);
+                helper(i+1 ,n, k , res , arr);
+                arr.remove(arr.size()-1);
         }
     }
 }
