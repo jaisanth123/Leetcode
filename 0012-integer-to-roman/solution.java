3fd518118ab322929523ceaafb20c9805e9bdb75
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-11-30 08:31:29 +0000

    [LeetCode Sync] Runtime - 3 ms (99.23%), Memory - 46.2 MB (65.23%)

--- 0012-integer-to-roman/solution.java
+++ 0012-integer-to-roman/solution.java
@@ -1,18 +1,16 @@
 class Solution {
     public String intToRoman(int num) {
-        StringBuilder sb = new StringBuilder();
-        int []nums ={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String []str ={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        for(int i = 0 ;i < nums.length;i++){
-            if(num ==0 ){
-                break;
+        StringBuilder res = new StringBuilder();
+        int [] val = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String [] roman = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        for(int i=0;i<val.length;i++){
+            if(num >= val[i]){
+                while(num>=val[i]){
+                res.append(roman[i]);
+                num-=val[i];
+                }
             }
-            while(num>=nums[i]){
-                sb.append(str[i]);
-                num-=nums[i];            
         }
-        }
-
-        return sb.toString();
+        return res.toString();
     }
 }
