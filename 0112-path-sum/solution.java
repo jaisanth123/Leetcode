3bd418ee52536883c1f56f10671776514ee947a7
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-12-04 09:08:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45 MB (16.77%)

--- 0112-path-sum/solution.java
+++ 0112-path-sum/solution.java
@@ -14,15 +14,26 @@
  * }
  */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        if (root == null) return false;
-
-        if (root.left == null && root.right == null) {
-            return targetSum - root.val == 0;
-        }
-
-        targetSum -= root.val;
-
-        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);        
+        boolean flag = false;
+    public boolean hasPathSum(TreeNode root, int ts) {
+        int sum =0;
+        
+        helper(root,ts,sum);
+        return flag;
+    }
+    public void helper(TreeNode root, int ts, int sum) {
+        if(flag==true)
+            return;
+        if(root==null )
+            return ;
+        
+        sum+=root.val;
+        if (sum ==ts && root.left==null && root.right==null){
+            flag=true;
+            return ;
+            }
+        
+        helper(root.left,ts,sum);
+        helper(root.right,ts,sum);
     }
 }
