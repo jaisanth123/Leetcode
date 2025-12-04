732cf8cd5ab5c6659ac05e29e1a2ca2df4c0de5c
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-12-04 15:57:48 +0000

    [LeetCode Sync] Runtime - 6 ms (54.60%), Memory - 48.3 MB (6.16%)

--- 0040-combination-sum-ii/solution.java
+++ 0040-combination-sum-ii/solution.java
@@ -1,35 +1,29 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> comb = new ArrayList<>();
+        int n = candidates.length;
         Arrays.sort(candidates);
-        backTrack(candidates , target , res , comb , 0);
+        helper(candidates,target,0,res,new ArrayList<>(),n);
+
         return res;
-
     }
-    private void backTrack(int[] candidates, int target, List<List<Integer>> res,List<Integer> comb , int c)
-    
-    {
-        if (target == 0)
-            {
-                res.add(new ArrayList<Integer>(comb));
-                return ;
+    private void helper(int[] candidates , int target , int index , List<List<Integer>> res , List<Integer> comb,int n){
+        if(target==0){
+            res.add(new ArrayList<>(comb));
+            return;
             }
-        if(target < 0)
-            return ;
-    
-        for(int i=c ; i < candidates.length ; i++){
-            
-            if(i>c && candidates[i]== candidates[i-1])
-                continue;
-            if(candidates[i]> target)
-                return;
-            comb.add(candidates[i]);
-            backTrack(candidates , target-candidates[i],res , comb , i+1);
-            comb.remove(comb.size() -1);
-            
-
-        }
         
+        if(target<0 || index >= n)
+            return;
+        for(int i = index ; i < n ; i++ ){
+             if(i>index && candidates[i]==candidates[i-1])
+            continue;
+            if(candidates[i] > target)
+            return;
+
+        comb.add(candidates[i]);
+        helper(candidates,target-candidates[i],i+1,res,comb,n);
+        comb.remove(comb.size() -1);
+        }
     }
 }
