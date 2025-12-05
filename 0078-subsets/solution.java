8389e14f61dda9c95503f49e2d742adaf4b5e756
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-12-05 00:27:00 +0000

    [LeetCode Sync] Runtime - 1 ms (78.55%), Memory - 44.1 MB (53.25%)

--- 0078-subsets/solution.java
+++ 0078-subsets/solution.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> combi = new ArrayList<>();
-        backtrack(nums , res, combi , 0 );
+        List<Integer> arr = new ArrayList<>();
+        helper(nums,res,arr,0);
         return res;
     }
-
-    private void backtrack(int [] nums , List<List<Integer>> res , List<Integer>combi, int c   ){
-
-        res.add(new ArrayList<Integer>(combi));
-
-        for(int i = c ; i < nums.length ; i++){
-            combi.add(nums[i]);
-            backtrack(nums , res, combi , i+1 );
-            combi.remove(combi.size()-1); 
+    private void helper (int [] nums , List<List<Integer>> res , List<Integer> arr , int start)
+    {
+        res.add(new ArrayList<>(arr));
+        if(start>=nums.length)
+            return;
+        for(int i = start ; i<nums.length;i++){
+                arr.add(nums[i]);
+                helper(nums,res,arr,i+1);
+                arr.remove(arr.size()-1);
         }
     }
-}
+    }
