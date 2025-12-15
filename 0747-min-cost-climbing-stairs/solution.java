a531fed2559a15fe4166a15a0fd70b4164753643
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-12-15 13:41:06 +0000

    [LeetCode Sync] Runtime - 1 ms (38.22%), Memory - 44.6 MB (72.67%)

--- 0747-min-cost-climbing-stairs/solution.java
+++ 0747-min-cost-climbing-stairs/solution.java
@@ -1,26 +1,17 @@
 class Solution {
-
-    private int helper(int cost[] , int memo[] , int n){
-        if(n<0)
-            return 0;
-        if(n<=1)
-            return cost[n];
-        if(memo[n]!=-1)
-            return memo[n];
-       return memo[n] =cost[n] + Math.min(helper(cost,memo,n-1),helper(cost,memo,n-2)); 
-    }
-
     public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        // int dp[] = new int [n];
-        // Arrays.fill(memo,-1);
-        // return Math.min(helper(cost,memo,n-1),helper(cost,memo,n-2));
-        // dp[0]=cost[0];
-        // dp[1] = cost[1];
-        for(int i = 2 ; i< n ; i++){
-            // dp[i] = cost[i]+Math.min(dp[i-1], dp[i-2]);
-            cost[i]+=Math.min(cost[i-1],cost[i-2]);
-        }
-        return Math.min(cost[n-1],cost[n-2]);
+        int memo[] = new int[cost.length+1];
+        Arrays.fill(memo,-1);
+       return Math.min(helper(memo,0,cost),helper(memo,1,cost));
     }
+    private static int helper(int [] memo ,int n, int[] cost )
+    {
+        if(n>= cost.length)
+            return 0;
+        if(memo[n]!= -1)
+            return memo[n];
+
+        return memo[n] = cost[n]+Math.min(helper(memo,n+1,cost),helper(memo,n+2,cost));
+    }
+
 }
