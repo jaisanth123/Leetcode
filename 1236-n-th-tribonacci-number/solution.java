0cf6c5147c5372ab612ad6f57de9189c6ef9d19d
Jaisanth K <154814877+jaisanth123@users.noreply.github.com>
2025-12-16 06:21:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (33.08%)

--- 1236-n-th-tribonacci-number/solution.java
+++ 1236-n-th-tribonacci-number/solution.java
@@ -1,17 +1,26 @@
 class Solution {
     public int tribonacci(int n) {
-        if(n<=1)
-            return n;
-        if(n==2)
+        if(n==0)
+            return 0;
+        if(n<=2)
             return 1;
-        int []dp = new int [n+1];
-        dp[0]=0;
+        int dp[] = new int[n+1];
+        dp[0] =0;
         dp[1]=1;
         dp[2]=1;
-        for(int i =3 ; i<n+1;i++){
+        for(int i = 3 ; i <= n ;i++)
             dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
-        }
         return dp[n];
         
+    }
+    private int helper(int n , int[]memo)
+    {
+        if(memo[n]!=-1)
+            return memo[n];
+        if(n==0)
+            return 0;
+        if(n<=2)
+            return 1;
+        return memo[n] = helper(n-1,memo)+helper(n-2,memo)+helper(n-3,memo);
     }
 }
